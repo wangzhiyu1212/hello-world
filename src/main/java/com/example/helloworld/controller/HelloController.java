2fdc00b797923787cc3c4dacb1c1aefe691535e0
mike <mike.wang@fwd.com>
2018-12-11 09:38:28 +0800

    redis practice

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -1,13 +1,30 @@
 package com.example.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+    StringRedisTemplate stringRedisTemplate;
+	
 	@RequestMapping("/hello")
 	public String getHello() {
         return "Hello World";
     }
+	
+	@RequestMapping("/redis")
+	public String getRedis() {
+		String source = stringRedisTemplate.opsForValue().get("mykey1");
+		System.out.println("source:" + source);
+        if (!StringUtils.isEmpty(source)) {
+            return source;
+        }
+        
+        return "Wrong";
+	}
 }
---
+++ src/main/java/com/example/helloworld/redis/RedisUtil.java
@@ -0,0 +1,5 @@
+package com.example.helloworld.redis;
+
+public class RedisUtil {
+
+}
