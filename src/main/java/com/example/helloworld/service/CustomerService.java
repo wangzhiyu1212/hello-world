07dcbddb5f0a9f72ff8e05ce9889347bb2d2c1de
mike <mike.wang@fwd.com>
2019-09-19 15:10:30 +0800

    mybatis

--- src/main/java/com/example/helloworld/service/CustomerService.java
+++ src/main/java/com/example/helloworld/service/CustomerService.java
@@ -16,33 +16,34 @@ import com.example.helloworld.mapper.OrderMapper;
 
 @Service
 public class CustomerService {
-	
-	private final static Logger		logger	= LoggerFactory.getLogger(CustomerService.class);
-	
+
+	private final static Logger logger = LoggerFactory.getLogger(CustomerService.class);
+
 	@Autowired
-    private RedisTemplate 			redisTemplate;
-	
+	private RedisTemplate redisTemplate;
+
 	@Autowired
-	private CustomerMapper 			customerMapper;
-	
-//	@Resource
-//    private OrderMapper 			orderMapper;
-	
+	private CustomerMapper customerMapper;
+
+	// @Resource
+	// private OrderMapper orderMapper;
+
 	@RequestMapping("/")
 	public String getName(Long id) {
-//		for (int i = 0; i<100; i++) {
-//			Order order = new Order();
-//	        order.setOrderId(redisTemplate.opsForValue().increment("OrderId", 1L));
-//	        order.setProductCode("redis");
-//	        orderMapper.insert(order);
-//		}
-		
-		for (int i = 0; i<100; i++) {
-			customerMapper.insertCustomer(redisTemplate.opsForValue().increment("CustomerId", 1L), "redis");
-			logger.info("i:" + i);
-		}
+		// for (int i = 0; i<100; i++) {
+		// Order order = new Order();
+		// order.setOrderId(redisTemplate.opsForValue().increment("OrderId", 1L));
+		// order.setProductCode("redis");
+		// orderMapper.insert(order);
+		// }
+
+		// for (int i = 0; i<100; i++) {
+		// customerMapper.insertCustomer(redisTemplate.opsForValue().increment("CustomerId",
+		// 1L), "redis");
+		// logger.info("i:" + i);
+		// }
 
 		return customerMapper.selectByPrimaryKey(id).getName();
-				
+
 	}
 }
