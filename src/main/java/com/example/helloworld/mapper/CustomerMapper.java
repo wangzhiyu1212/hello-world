0e96780f0aa739006dde2beb5de6933a2df7d6c9
wangl <mike.wang@fwd.com>
2019-12-27 17:18:47 +0800

    33

--- src/main/java/com/example/helloworld/mapper/CustomerMapper.java
+++ src/main/java/com/example/helloworld/mapper/CustomerMapper.java
@@ -1,16 +1,9 @@
 package com.example.helloworld.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-
 import com.example.helloworld.entity.Customer;
 
 public interface CustomerMapper {
     int insert(Customer record);
 
     int insertSelective(Customer record);
-    
-    @Insert({"INSERT INTO customer (customer_id,name,sex,poinsts) VALUES (#{customerId},#{name},'M',100)"})
-    int insertCustomer(@Param("customerId")Long customerId, @Param("name")String name);
-    
 }
