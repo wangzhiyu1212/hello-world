fcdddc3ae24ce7b6c076daa2d4c7df190d97ac36
wangl <mike.wang@fwd.com>
2020-01-21 10:01:10 +0800

    增加cron

---
+++ src/main/java/com/example/helloworld/job/CronJob.java
@@ -0,0 +1,20 @@
+package com.example.helloworld.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.example.helloworld.controller.HelloController;
+
+@Component
+public class CronJob {
+	private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+	@Scheduled(cron = "0 0/1 * * * ?")
+	public void cronJob() throws InterruptedException {
+
+		logger.info("cronJob start");
+
+	}
+}
