328f7362f31a880b4c54b6c73662b9930f38dfb7
wangzhiyu1212 <wangzhiyu1616@163.com>
2020-08-24 15:40:32 +0800

    commit

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -47,136 +47,137 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "初始接口测试页面")
 @RestController
 public class HelloController {
-	private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-	@Autowired
-	private RedisTemplate<String, Map<String, Object>> redisTemplate;
+    @Autowired
+    private RedisTemplate<String, Map<String, Object>> redisTemplate;
 
-	@Autowired
-	private CustomerService customerService;
+    @Autowired
+    private CustomerService customerService;
 
-	@Resource
-    private PolicyInfoMapper policyMapper;
-	
-	// @Autowired
-	// private UserRepository userRepository;
-	//
-	// @Autowired
-	// private MongoDbFactory mongoDbFactory;
-	//
-	// @Autowired
-	// private GridFsTemplate gridFsTemplate;
+    // @Resource
+    // private PolicyInfoMapper policyMapper;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/hello")
-	public String getHello(HttpServletRequest httpRequest) {
-		return "Hello World";
-	}
+    // @Autowired
+    // private UserRepository userRepository;
+    //
+    // @Autowired
+    // private MongoDbFactory mongoDbFactory;
+    //
+    // @Autowired
+    // private GridFsTemplate gridFsTemplate;
 
-	@RequestMapping(method = RequestMethod.GET, value = "/get")
-	@ApiOperation(value = "获取查询参数")
-	public String getHello(@RequestParam(name = "name") String name, HttpServletRequest httpRequest) {
+    @RequestMapping(value = "/hello", method = { RequestMethod.POST, RequestMethod.GET })
+    public String getHello(HttpServletRequest httpRequest) {
+        return "Hello World";
+    }
 
-		return "Hello, " + name;
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/get")
+    @ApiOperation(value = "获取查询参数")
+    public String getHello(@RequestParam(name = "name") String name, HttpServletRequest httpRequest) {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/hello/{name}")
-	@ApiOperation(value = "获取路径参数")
-	public String hello(@PathVariable("name") String name) {
-		return "Hello, " + name;
-	}
+        return "Hello, " + name;
+    }
 
-	@RequestMapping(value = "/savePolicyInfo", method = { RequestMethod.POST, RequestMethod.GET })
-	public String savePolicyInfo(@RequestBody PolicyInfo policyInfo) {
-		for (int i = 0; i < 1000; i++) {
-			policyMapper.insert(policyInfo);
-		}
-		return "完成";
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/hello/{name}")
+    @ApiOperation(value = "获取路径参数")
+    public String hello(@PathVariable("name") String name) {
+        return "Hello, " + name;
+    }
 
-	// @RequestMapping("/sso")
-	// public String getUsername(HttpServletRequest httpRequest, HttpServletResponse
-	// httpResponse) {
-	// return LdapAuthUtil.getADUsername(httpRequest, httpResponse);
-	// }
+    // @RequestMapping(value = "/savePolicyInfo", method = { RequestMethod.POST,
+    // RequestMethod.GET })
+    // public String savePolicyInfo(@RequestBody PolicyInfo policyInfo) {
+    // for (int i = 0; i < 1000; i++) {
+    // policyMapper.insert(policyInfo);
+    // }
+    // return "完成";
+    // }
 
-	@RequestMapping(method = RequestMethod.POST, value = "/redis")
-	public String getRedis() {
-		try {
-			Map<String, Object> testMap = new HashMap<String, Object>();
-			Map<String, Object> testMap2 = new HashMap<String, Object>();
-			testMap2.put("test2", "mike");
-			testMap.put("name", "jack");
-			testMap.put("age", 27);
-			testMap.put("class", "1");
-			testMap.put("in", testMap2);
-			redisTemplate.opsForHash().putAll("redishash", testMap);
-			logger.info(redisTemplate.opsForHash().entries("redishash").toString());
-			logger.info(redisTemplate.opsForHash().get("redishash", "age").toString());
-			return String.valueOf(redisTemplate.opsForValue().increment("SSOsessionId", 1L));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    // @RequestMapping("/sso")
+    // public String getUsername(HttpServletRequest httpRequest, HttpServletResponse
+    // httpResponse) {
+    // return LdapAuthUtil.getADUsername(httpRequest, httpResponse);
+    // }
 
-		return "empty";
-	}
+    @RequestMapping(method = RequestMethod.POST, value = "/redis")
+    public String getRedis() {
+        try {
+            Map<String, Object> testMap = new HashMap<String, Object>();
+            Map<String, Object> testMap2 = new HashMap<String, Object>();
+            testMap2.put("test2", "mike");
+            testMap.put("name", "jack");
+            testMap.put("age", 27);
+            testMap.put("class", "1");
+            testMap.put("in", testMap2);
+            redisTemplate.opsForHash().putAll("redishash", testMap);
+            logger.info(redisTemplate.opsForHash().entries("redishash").toString());
+            logger.info(redisTemplate.opsForHash().get("redishash", "age").toString());
+            return String.valueOf(redisTemplate.opsForValue().increment("SSOsessionId", 1L));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	@RequestMapping(value = "/mybatis")
-	public String getCustomer() {
+        return "empty";
+    }
 
-		return customerService.insertCustomer();
-	}
+    @RequestMapping(value = "/mybatis")
+    public String getCustomer() {
 
-	// @RequestMapping("/mongodb/getAll")
-	// public List<User> getAllUser() {
-	// List<User> userList = userRepository.findAll();
-	// return userList;
-	// }
-	//
-	// @RequestMapping("/mongodb/savePic")
-	// public String savePic() throws FileNotFoundException {
-	// File file = new File("D:/test.png");
-	// // 定义输入流
-	// FileInputStream inputStram = new FileInputStream(file);
-	// // 向GridFS存储文件
-	// ObjectId objectId = gridFsTemplate.store(inputStram, file.getName(), "");
-	// // 得到文件ID
-	// String fileId = objectId.toString();
-	//
-	// return fileId;
-	// }
+        return customerService.insertCustomer();
+    }
 
-	// private byte[] getBytes(InputStream inputStream) throws Exception {
-	// ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	// byte[] b = new byte[1024];
-	// int i = 0;
-	// while (-1 != (i = inputStream.read(b))) {
-	// bos.write(b, 0, i);
-	// }
-	// return bos.toByteArray();
-	// }
+    // @RequestMapping("/mongodb/getAll")
+    // public List<User> getAllUser() {
+    // List<User> userList = userRepository.findAll();
+    // return userList;
+    // }
+    //
+    // @RequestMapping("/mongodb/savePic")
+    // public String savePic() throws FileNotFoundException {
+    // File file = new File("D:/test.png");
+    // // 定义输入流
+    // FileInputStream inputStram = new FileInputStream(file);
+    // // 向GridFS存储文件
+    // ObjectId objectId = gridFsTemplate.store(inputStram, file.getName(), "");
+    // // 得到文件ID
+    // String fileId = objectId.toString();
+    //
+    // return fileId;
+    // }
 
-	// @RequestMapping(value = "/mongodb/show")
-	// public void show(HttpServletRequest request, HttpServletResponse response) {
-	// try {
-	// OutputStream out = response.getOutputStream();
-	// response.setContentType("image/jpg");
-	//
-	// GridFSFile file = gridFsTemplate
-	// .findOne(new Query().addCriteria(Criteria.where("filename").is("test.png")));
-	//
-	// GridFSBucket bucket = GridFSBuckets.create(mongoDbFactory.getDb());
-	// GridFSDownloadStream gridFSDownloadStream =
-	// bucket.openDownloadStream(file.getObjectId());
-	//
-	// GridFsResource resource = new GridFsResource(file, gridFSDownloadStream);
-	//
-	// InputStream inputStream = resource.getInputStream();
-	// byte[] f = getBytes(inputStream);
-	//
-	// out.write(f);
-	//
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
+    // private byte[] getBytes(InputStream inputStream) throws Exception {
+    // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    // byte[] b = new byte[1024];
+    // int i = 0;
+    // while (-1 != (i = inputStream.read(b))) {
+    // bos.write(b, 0, i);
+    // }
+    // return bos.toByteArray();
+    // }
+
+    // @RequestMapping(value = "/mongodb/show")
+    // public void show(HttpServletRequest request, HttpServletResponse response) {
+    // try {
+    // OutputStream out = response.getOutputStream();
+    // response.setContentType("image/jpg");
+    //
+    // GridFSFile file = gridFsTemplate
+    // .findOne(new Query().addCriteria(Criteria.where("filename").is("test.png")));
+    //
+    // GridFSBucket bucket = GridFSBuckets.create(mongoDbFactory.getDb());
+    // GridFSDownloadStream gridFSDownloadStream =
+    // bucket.openDownloadStream(file.getObjectId());
+    //
+    // GridFsResource resource = new GridFsResource(file, gridFSDownloadStream);
+    //
+    // InputStream inputStream = resource.getInputStream();
+    // byte[] f = getBytes(inputStream);
+    //
+    // out.write(f);
+    //
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // }
 }
